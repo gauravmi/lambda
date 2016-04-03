d44f85cff8be2ee1bce033e050dbd019accf38ed
GAURAV MANOHARRAV INGALKAR <gauravmi@thoughtworks.com>
2016-04-03 23:06:57 +0530

    add and multiply :)

---
+++ fun.js
@@ -0,0 +1,42 @@
+function print(arg) {
+	console.log(test_integer(arg))
+}
+
+function test_integer(number) {
+	return number(function(n) {
+		return n + 1
+	})(0)
+}
+
+
+// SQUARE = function(n){
+// 	return function(s) { return function(i){ return s(s(i)) } }(n)
+// }
+
+
+print(function(g){ return function(h){ return function(m){ return function(n){
+    return function(z){ return h(g(m)(n))(z); } } } }
+}(function(m) { return function(n) { return m(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) {
+   return s(fn_number(s)(i)); } } })(n); }
+}(n))(function(s) { return function(i){ return i } }); }
+})(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i));
+  }
+ } })(n); }
+})(function(m) { return function(n) { return m(function(fn_number) {
+ return function(s) { return function(i) { return s(fn_number(s)(i)); }
+ } })(n); }
+}(function(s) { return function(i){ return s(s(s(i))) } })(function(s) { return function(i){ return s(s(i)) } }))(function(m) { return function(n) {
+  return m(function(fn_number) { return function(s) { return function(i) {
+   return s(fn_number(s)(i)); } } })(n); }
+}(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i)); }
+ } })(n); }
+}(function(s) { return function(i){ return s(s(s(i))) } })(function(s) { return function(i){ return s(s(i)) } }))(function(s) { return function(i){ return s(s(i)) } }))(function(m) { return function(n) { return m(function(m) { return function(n) { return m(function(fn_number) { return function(s) {
+  return function(i) { return s(fn_number(s)(i)); } } })(n); } }(n))(function(s) { return function(i){ return i } }); } }(function(m) {
+ return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i)); } }
+})(n); } }(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i)); }
+ } })(n); } }(function(s) { return function(i){ return s(s(s(i))) } })(function(s) { return function(i){ return s(s(i)) } }))(function(m) {
+ return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i)); } } })(n); }
+}(function(s) { return function(i){ return s(s(s(i))) } })(function(s) { return  function(i){ return s(s(i)) } })))(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) {
+   return s(fn_number(s)(i)); } } })(n); } }(function(m) { return function(n) { return m(function(fn_number) { return function(s) { return function(i) { return s(fn_number(s)(i)); } } })(n);
+ } }(function(s) { return function(i){ return s(s(s(i))) } })(function(s) { return function(i){ return s(s(i)) } }))(function(s) { return function(i){ return s(s(i)) } })))
+)
